259802ce436aad90bc9e30b10e70b8614ca0506c
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-10-14 19:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+	const radius=document.getElementById("radius").value;
+	let volume=(4/3)*3.14*radius*radius;
+	document.getElementById("volume").value=volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
